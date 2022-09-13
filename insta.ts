e037d10e7f48ce0a51749af7f7e95f2e33ad3f9c
flynnson <flynn.fawa@gmail.com>
2022-09-13 15:57:51 -0700

    making more beautiful code

--- insta.ts
+++ insta.ts
@@ -1,9 +1,9 @@
-import axiod from "https://deno.land/x/axiod/mod.ts"
+import axiod from "https://deno.land/x/axiod/mod.ts";
 
-export default async function getLink(url: string ){
-	const response = await axiod.get(url)
+export default async function getLink(url: string) {
+  const response = await axiod.get(url);
 
-	let video_link = response.data.url[0].url
+  let video_link = response.data.url[0].url;
 
-	return video_link
+  return video_link;
 }
